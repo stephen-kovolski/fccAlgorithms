87f48df0fe58f64f3eb4c78bc4075ec6e164f80b
stephen-kovolski <s.kovolski@gmail.com>
2022-03-16 22:28:20 -0400

    .

---
+++ reverse_a_string.js
@@ -0,0 +1,18 @@
+// Reverse the provided string.
+// You may need to turn the string into an array before you can reverse it.
+// Your result must be a string.
+// Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+function reverseString(str) {
+  
+  for (let i = str.length-1; i > 0; i--){
+
+    console.log(str[i])
+
+  }
+
+
+
+}
+
+reverseString("hello");
