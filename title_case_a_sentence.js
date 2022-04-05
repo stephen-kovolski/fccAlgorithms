6b17c87212e684157c046e23cb48b284bc7c4d88
stephen-kovolski <s.kovolski@gmail.com>
2022-04-04 22:27:27 -0400

    solved

--- title_case_a_sentence.js
+++ title_case_a_sentence.js
@@ -2,19 +2,11 @@
 // For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
 
 function titleCase(str) {
-    let split_words = str.split(" ");
-
-    //split_words.push(new_string);
-
-    //console.log(split_words)
-
-    for (i = 0; i < split_words; i++){
-        let new_array = [];
-        split_words[i] += new_array
-        console.log(new_array)
-
-        }
-    
+    return str
+    .toLowerCase()
+    .split(" ")
+    .map(val => val.replace(val.charAt(0), val.charAt(0).toUpperCase()))
+    .join(" ");
 
 }
 
