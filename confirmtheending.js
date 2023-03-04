3a8ccd073e6eb3bab83d6a4cff6fedc686aa02e6
stephen-kovolski <s.kovolski@gmail.com>
2023-03-03 23:03:05 -0500

    solved 3.3.23

--- confirmtheending.js
+++ confirmtheending.js
@@ -1,25 +1,31 @@
-// function confirmEnding(str, target){
+/*
 
-// if (str[str.length - 1] == target){
-//   return(true)
-//   }else {
-//     return(false){
-//   }
-// }
 
-//   confirmEnding("bastian", "n")
-  
+Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
+
+*/
+
+//pop off the last 'amount' to an array and compare the two
+
 
 function confirmEnding(str, target) {
-  const strLastLetters = str.substring(str.length-target.length)
-    if (strLastLetters == target) {
-      return true;
-    } else {
-      return false;
-    }
+
+  let amount = target.length;
+ 
+  
+  if (str[str.length-1] == target){
+    console.log('true');
+  }else{
+    console.log('false')
+  }
 
 }
 
 confirmEnding("Bastian", "n");
-
-
+confirmEnding("Joey", "y");
+confirmEnding("Chrissy", "a");
+confirmEnding("Stephen", "K");
+confirmEnding("Giggsy", "y");
+confirmEnding("Gram", "5");
