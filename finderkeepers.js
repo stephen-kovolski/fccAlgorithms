c4649f40841bbddc10f9680b92c6ceae3f35ef3b
stephen-kovolski <s.kovolski@gmail.com>
2023-03-07 22:36:28 -0500

    3.7

---
+++ finderkeepers.js
@@ -0,0 +1,16 @@
+//Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'. This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.
+
+
+function findElement(arr, func) {
+    let num = [];
+    
+    for (let i = 0; i < arr.length; i ++){
+        if (arr[i] % 2 === 0){
+            num = arr[i]
+        }
+    }
+
+    console.log(num)
+  }
+  
+  findElement([1, 2, 3, 4]);
