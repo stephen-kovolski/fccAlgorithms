0030da3180b5899df865e456b0f9d2c722582f77
stephen-kovolski <s.kovolski@gmail.com>
2023-03-07 23:07:46 -0500

    solved 3.7

--- finderkeepers.js
+++ finderkeepers.js
@@ -6,11 +6,14 @@ function findElement(arr, func) {
     
     for (let i = 0; i < arr.length; i ++){
         if (arr[i] % 2 === 0){
-            num = arr[i]
+            num.push( arr[i])
         }
+        
     }
 
     console.log(num)
   }
   
   findElement([1, 2, 3, 4]);
+  findElement([1, 3, 5, 8, 9, 10]);
+  findElement([1, 3, 5, 9]);
