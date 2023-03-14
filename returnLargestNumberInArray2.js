a363e4f6853bc7e82177f0abf22e0e6a93ce2538
stephen-kovolski <s.kovolski@gmail.com>
2023-03-13 20:45:36 -0400

    added notes - solved -

--- returnLargestNumberInArray2.js
+++ returnLargestNumberInArray2.js
@@ -10,14 +10,14 @@ function largestOfFour(arr) {
 
     let new_array = []; //the arary Im going to put the biggest numbers into
 
-    for (let i = 0; i < arr.length; i++){  //outer loop
-        let bigNum = 1;  //new variable to hold big numbers from arr[i]
-        for (let j = 0; j < arr[i].length; j++){ //inner loop
-             if (arr[i][j] > bigNum){ //
-                 bigNum = arr[i][j]
+    for (let i = 0; i < arr.length; i++){  //outer loop. Allows the next loop (j) to go insode each of the 4 arrays
+        let bigNum = 0;  //new variable to hold big numbers from arr[i]
+        for (let j = 0; j < arr[i].length; j++){ //inner loop that goes insode of the four arrays
+             if (arr[i][j] > bigNum){ //the number j is on, if thats bigger that the number in bigNum than bigNum becomes equal to j.  And so on until it finds the niggest number in the array.
+                 bigNum = arr[i][j] //mentioned in notes directly above
              }
             
-            new_array[i] = bigNum;
+            new_array[i] = bigNum; //this number iterates through and puts all of the numbers that were in bigNum into newArray
 
 
             }
