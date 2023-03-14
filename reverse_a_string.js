50b6b7b24dac1e7ce4d1c6b67309741f4b2fe527
stephen-kovolski <s.kovolski@gmail.com>
2023-03-13 21:35:39 -0400

    solved 3.13

--- reverse_a_string.js
+++ reverse_a_string.js
@@ -4,17 +4,16 @@
 // Remember to use Read-Search-Ask if you get stuck. Write your own code.
 
 function reverseString(str) {
+
+  let new_array = [];
   
   for (let i = str.length-1; i >= 0; i--){
-
-    new_string = str[i]
-
-    console.log(new_string)
-
+    new_array.push(str[i])
   }
-
+  //console.log(str.substring(str.length, str[0]));
+  console.log(new_array)
 
 
 }
 
-reverseString("good day mate");
+reverseString("hello");
