4b36fd8eae9c1a0f1fcaff63365de7fbf9d2ed7b
stephen-kovolski <s.kovolski@gmail.com>
2023-03-15 22:11:59 -0400

    solved

--- slice_and_splice.js
+++ slice_and_splice.js
@@ -5,13 +5,35 @@
 // Remember to use Read-Search-Ask if you get stuck. Write your own code.
 
 function frankenSplice(arr1, arr2, n) {
+    let array_one = arr2;
+    array_one.splice(n, 0, arr1);
     
-    //new_array = [];
-    arr2.splice(n, 0, arr1)
-    console.log(arr2)
-  }
-  
+    console.log(array_one)
+}
   frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //new_array = [];
+    
+  //    for (let i = 0; i < arr1.length; i++){
+  //         arr2.splice(n, 0, arr1[i])
+  //    }
+  //     console.log(arr2)
 
 
 
