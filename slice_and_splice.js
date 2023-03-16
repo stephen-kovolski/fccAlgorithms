1f94e530e38ffe597585a2fd46fd20c4e1ea8685
stephen-kovolski <s.kovolski@gmail.com>
2023-03-15 20:55:18 -0400

    finished

--- slice_and_splice.js
+++ slice_and_splice.js
@@ -5,14 +5,13 @@
 // Remember to use Read-Search-Ask if you get stuck. Write your own code.
 
 function frankenSplice(arr1, arr2, n) {
-
-    let newArr2 = [...arr2];
-    newArr2.splice(n, 0, ...arr1);
-    console.log(newArr2);
-
-}
-
-frankenSplice([1, 2, 3], [4, 5, 6], 1);
+    
+    //new_array = [];
+    arr2.splice(n, 0, arr1)
+    console.log(arr2)
+  }
+  
+  frankenSplice([1, 2, 3], [4, 5, 6], 1);
 
 
 
