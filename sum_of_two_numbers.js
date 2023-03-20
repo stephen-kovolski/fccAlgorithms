2f70e70f599bb73d199b830c1f83b5d16816df89
stephen-kovolski <s.kovolski@gmail.com>
2023-03-20 08:43:44 -0400

    initial commit

---
+++ sum_of_two_numbers.js
@@ -0,0 +1,26 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1]
+*/
+
+
+function twoSum(nums, target) {
+
+    let numbers;
+    
+    for (let i = 0; i < nums.length; i++){
+        for (let j = 0; j < nums[i].length; j++){
+            
+        }
+        
+}
+
+};
+
+twoSum([5, 7, 9, 11], 20)
