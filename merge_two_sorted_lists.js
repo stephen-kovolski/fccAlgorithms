b28ec9f52df2c2778bbe76f799e9c38edf531436
stephen-kovolski <s.kovolski@gmail.com>
2023-04-02 22:56:31 -0400

    solved 4.2

--- merge_two_sorted_lists.js
+++ merge_two_sorted_lists.js
@@ -1,20 +1,16 @@
 /*
 You are given the heads of two sorted linked lists list1 and list2.
-
 Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
 
 Example 1:
-
 Input: list1 = [1,2,4], list2 = [1,3,4]
 Output: [1,1,2,3,4,4]
 
 Example 2:
-
 Input: list1 = [], list2 = []
 Output: []
 
 Example 3:
-
 Input: list1 = [], list2 = [0]
 Output: [0]
 
@@ -35,3 +31,5 @@ mergeTwoLists([1,2,4], [1,3,4])
 mergeTwoLists([9, 13, 56], [17, 97, 35])
 mergeTwoLists([54, -1, 2], [104, 67, 0])
 mergeTwoLists([0, 2, 1], [0, 1, 3])
+mergeTwoLists([], [])
+mergeTwoLists([], [0])
