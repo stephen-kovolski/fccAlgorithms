60c639b7f847ad4aa8c2058cade429f4fb0e60eb
stephen-kovolski <s.kovolski@gmail.com>
2023-04-02 22:50:39 -0400

    solved 4.2

--- merge_two_sorted_lists.js
+++ merge_two_sorted_lists.js
@@ -21,11 +21,16 @@ Output: [0]
 
 */
 
-var mergeTwoLists = function(list1, list2) {
 
 
-    
+function mergeTwoLists(list1, list2) {
+    let new_array = [];
+    new_array.push(list1, list2)
+    let flat_array = new_array.flat(1)
+    flat_array.sort(function(a, b) {return a - b;})
+    console.log(flat_array);
 };
 
-
 mergeTwoLists([1,2,4], [1,3,4])
+mergeTwoLists([9, 13, 56], [17, 97, 35])
+mergeTwoLists([54, -1, 2], [104, 67, 0])
