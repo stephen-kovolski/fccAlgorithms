fedf3dd1aab4886740a54eee7b7cac520f28bab5
stephen-kovolski <s.kovolski@gmail.com>
2023-04-03 08:44:29 -0400

    init

---
+++ remove_duplicates.js
@@ -0,0 +1,23 @@
+/*
+Remove duplicates from a sorted list.
+EXAMPLE 1
+Input: head = [1,1,2]
+Output: [1,2]
+
+EXAMPLE 2
+Input: head = [1,1,2,3,3]
+Output: [1,2,3]
+*/
+
+
+
+function deleteDuplicates(param) {
+
+
+    
+};
+
+
+deleteDuplicates([1,1,2,3,3])
+deleteDuplicates([1,1,2])
+deleteDuplicates([6, 23, 75, 234, 76, 23, 0, 54, 23, 96, 23])
