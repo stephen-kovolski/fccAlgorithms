d9712001140a1dbe8af23d2055df5ec087475f9a
stephen-kovolski <s.kovolski@gmail.com>
2023-04-03 23:14:39 -0400

    progressing

--- remove_duplicates.js
+++ remove_duplicates.js
@@ -9,15 +9,26 @@ Input: head = [1,1,2,3,3]
 Output: [1,2,3]
 */
 
-
+let first_num = [];
+let second_num;
 
 function deleteDuplicates(param) {
 
+    for (let i =0; i < param.length; i++){
+        for (let j =1; j < param.length; j++){
+            
+            if (param[i]-param[j] == 0){
+                param.slice(i)
+            }
 
-    
+        }
+
+    }
+    console.log(first_num);
+
 };
 
 
 deleteDuplicates([1,1,2,3,3])
-deleteDuplicates([1,1,2])
-deleteDuplicates([6, 23, 75, 234, 76, 23, 0, 54, 23, 96, 23])
+// deleteDuplicates([1,1,2])
+// deleteDuplicates([6, 23, 75, 234, 76, 23, 0, 54, 23, 96, 23])
