e0b5ac178ebb7dff8c1930a7eeedfe589a8be1da
stephen-kovolski <s.kovolski@gmail.com>
2023-05-21 11:00:06 -0400

    string

--- spinalTap.js
+++ spinalTap.js
@@ -3,9 +3,14 @@ Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-da
 */
 
 function spinalCase(str) {
+    let newString;
+
+    str.newstring()
+
+
     return str;
   }
   
   spinalCase('This Is Spinal Tap');
-  spinalCase("thisIsSpinalTap")
-  spinalCase("The_Andy_Griffith_Show")
+//   spinalCase("thisIsSpinalTap")
+//   spinalCase("The_Andy_Griffith_Show")
