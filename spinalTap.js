3595031674f4f66ca7db2310be4344fa2a29cc85
stephen-kovolski <s.kovolski@gmail.com>
2023-05-20 23:45:03 -0400

    func

---
+++ spinalTap.js
@@ -0,0 +1,11 @@
+/*
+Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+*/
+
+function spinalCase(str) {
+    return str;
+  }
+  
+  spinalCase('This Is Spinal Tap');
+  spinalCase("thisIsSpinalTap")
+  spinalCase("The_Andy_Griffith_Show")
